b8d0da37969a45e83b605c5165b057bb3bda8909
Mxk01 <RLuntraru22@gmail.com>
2023-06-04 08:15:55 +0300

    =

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const {Server} = require("socket.io");
 const io = new Server(server,{
     cors : {
         // origin is where we accept requests from
-        origin :'https://nexo-talk.onrender.com/',
+        origin :'*',
         methods:['GET','POST'],
         credentials:true
     }
